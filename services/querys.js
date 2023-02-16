8f8c3b7e3ccf36cf68aad154a1f9cd5be28c7bef
tuNovio <alfgomachi@gmail.com>
2023-02-16 18:31:46 -0500

    inicialización de las rutas

--- services/querys.js
+++ services/querys.js
@@ -10,19 +10,13 @@ async function testUser() {
 }
 
 async function createUser() {
-    try {
         const nuevo = User.build({
             Nombre: 'Alfonso',
             Apellido: 'Gomajoa',
             Edad: 23
         })
-        console.log(nuevo instanceof User)
-        console.log(nuevo.Nombre)
         await nuevo.save()
         console.log("guardando: ", nuevo)
-    } catch (err) {
-        console.error(`error: ${err.message}`)
-    }
 }
 
 testUser()
