cba5e914273b1c9ded111bce0d8c96a0c38fd963
tuNovio <alfgomachi@gmail.com>
2023-03-31 20:00:30 -0500

    change single quete for double quote

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const path = require('path')
+const express = require("express")
+const path = require("path")
 const app = express()
 
 
-const userRoutes = require('./src/routes/user')
-const cuentaRoutes = require('./src/routes/cuenta')
+const userRoutes = require("./src/routes/user")
+const cuentaRoutes = require("./src/routes/cuenta")
 
 
-app.set('port', 3000)
+app.set("port", 3000)
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs")
 
-app.set('views', path.join(__dirname, './src/views'))
+app.set("views", path.join(__dirname, "./src/views"))
 
-app.use(express.static('./public'))
+app.use(express.static("./public"))
 
 app.use(express.json())
 
@@ -29,6 +29,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(userRoutes)
 app.use(cuentaRoutes)
 
-app.listen(app.get('port'), () => {
-    console.log(`Puerto excucha en:  ${app.get('port')}`)
+app.listen(app.get("port"), () => {
+    console.log(`Puerto excucha en:  ${app.get("port")}`)
 })
