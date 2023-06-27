48dfcf03aa682f25c0e4888156889f64d1e1203f
tuNovio <alfgomachi@gmail.com>
2023-06-26 20:59:21 -0500

    comentarios

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
+//Inicializa dependencias 
 const express = require("express")
 const path = require("path")
+//instalcia de server express
 const app = express()
 
-
+//rutas
 const userRoutes = require("./src/routes/user")
 const cuentaRoutes = require("./src/routes/cuenta")
 
-
+//configuración
 app.set("port", 3000)
-
+//engine server side
 app.set("view engine", "ejs")
-
+//path de las paginas server side
 app.set("views", path.join(__dirname, "./src/views"))
-
+//archivos publicos
 app.use(express.static("./public"))
-
+//rederizade de archivos json
 app.use(express.json())
-
+//uncode enable
 app.use(express.urlencoded({ extended: true }))
 
-
+//inicializar las rutas
 app.use(userRoutes)
 app.use(cuentaRoutes)
-app
+//test del server
+app.get('/', (req, res) => {
+    res.send("Server inicializado")
+})
 app.listen(app.get("port"), () => {
     console.log(`Puerto excucha en:  ${app.get("port")}`)
 })
